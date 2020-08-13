18d87b27f3284ffa8925c4db792ae9d74ccd2f88
Denis Willm <DennisWillm@t-online.de>
2020-08-13 20:13:26 +0200

    k

--- src/rewardLocations/critical.ts
+++ src/rewardLocations/critical.ts
@@ -1,6 +1,9 @@
 import { LocationName } from "../LocationName";
 import { Rewards } from "../rewards";
 import { RewardLocation, RewardLocationType } from "./RewardLocation";
+import { GameMode } from "src/settings/enums";
+import { modifierUpgrades } from "src/patches/modifierUpgrades";
+import { RewardType } from "src/rewards/Reward";
 
 export const criticalRewardLocations: RewardLocation[] = [
 	{
@@ -8,7 +11,12 @@ export const criticalRewardLocations: RewardLocation[] = [
 		location: LocationName.CRITICAL,
 		description: "Bonus 1",
 		value: "11D18DDE",
-		reward: Rewards.REACTION_BOOST,
+		reward: Rewards.REACTION_BOOST
+		gameMode: {
+			[GameMode.GOA_MOD]: {
+				exludeType: [RewardType.BONUS_MODIFIER]
+			},
+		},
 	},
 	{
 		type: RewardLocationType.CRITICAL,
@@ -16,6 +24,11 @@ export const criticalRewardLocations: RewardLocation[] = [
 		description: "Bonus 2",
 		value: "11D18DDC",
 		reward: Rewards.FINISHING_PLUS,
+		gameMode: {
+			[GameMode.GOA_MOD]: {
+				exludeType: [RewardType.BONUS_MODIFIER]
+			},
+		},
 	},
 	{
 		type: RewardLocationType.CRITICAL,
@@ -23,6 +36,11 @@ export const criticalRewardLocations: RewardLocation[] = [
 		description: "Bonus 3",
 		value: "11D18DE8",
 		reward: Rewards.DRAW,
+		gameMode: {
+			[GameMode.GOA_MOD]: {
+				exludeType: [RewardType.BONUS_MODIFIER]
+			},
+		},
 	},
 	{
 		type: RewardLocationType.CRITICAL,
@@ -30,6 +48,11 @@ export const criticalRewardLocations: RewardLocation[] = [
 		description: "Bonus 4",
 		value: "11D18DE4",
 		reward: Rewards.LUCKY_LUCKY,
+		gameMode: {
+			[GameMode.GOA_MOD]: {
+				exludeType: [RewardType.BONUS_MODIFIER]
+			},
+		},
 	},
 	{
 		type: RewardLocationType.CRITICAL,
@@ -37,6 +60,11 @@ export const criticalRewardLocations: RewardLocation[] = [
 		description: "Bonus 5",
 		value: "11D18DE6",
 		reward: Rewards.LUCKY_LUCKY,
+		gameMode: {
+			[GameMode.GOA_MOD]: {
+				exludeType: [RewardType.BONUS_MODIFIER]
+			},
+		},
 	},
 	{
 		type: RewardLocationType.CRITICAL,
@@ -44,12 +72,21 @@ export const criticalRewardLocations: RewardLocation[] = [
 		description: "Bonus 6",
 		value: "11D18DE0",
 		reward: Rewards.MP_HASTERA,
+		gameMode: {
+			[GameMode.GOA_MOD]: {
+				exludeType: [RewardType.BONUS_MODIFIER]
+			},
+		},
 	},
 	{
 		type: RewardLocationType.CRITICAL,
 		location: LocationName.CRITICAL,
 		description: "Bonus 7",
 		value: "11D18DE2",
-		reward: Rewards.NO_EXPERIENCE,
+		reward: Rewards.NO_EXPERIENCE,gameMode: {
+			[GameMode.GOA_MOD]: {
+				exludeType: [RewardType.BONUS_MODIFIER]
+			},
+		},
 	},
 ];
